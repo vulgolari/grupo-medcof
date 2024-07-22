cb6c45f60a37841d2c6b7d8a7995686f20722390
Larissa Dias <vulgolari@hotmail.com>
2024-07-22 17:14:29 -0300

    Melhora de código

--- cypress/integration/cypress/e2e/spec.cy.js
+++ cypress/integration/cypress/e2e/spec.cy.js
@@ -1,16 +1,20 @@
 describe('Teste de interface do Grupo Medcof', () => {
-  it('Executa o cenário de teste no site', () => {
-    // Passo 1: Abrir o site
+  // Executa antes de cada teste no bloco
+  beforeEach(() => {
+    // Visita a página inicial e navega para a seção de cursos
     cy.visit('https://www.grupomedcof.com.br/home/');
-
-    // Passo 2: Navegar para a seção de cursos
-    cy.get('a[href*="#cursos-section"]').click();
-
-    // Passo 3: Verificar se a URL correta está presente
+    cy.get('a[href*="#cursos-section"]').should('be.visible').click();
     cy.url().should('include', '/home/#cursos-section');
+  });
 
-    // Passo 4: Selecionar o curso HIIT
-    cy.visit('https://hiit.grupomedcof.com.br/r1-acesso-direto');
-    cy.contains('Quero Participar do HIIT Agora').should('exist');
+  it('Verifica a navegação até a seção de cursos', () => {
+    cy.url().should('include', '/home/#cursos-section');
+  });
+
+  it('Seleção de um curso específico', () => {
+    cy.contains('HIIT R1').should('be.visible').click();
   });
 });
+
+
+
